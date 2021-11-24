0081d907f30f89e2ee03d1c5da5a05c63f4893a4
valtteri <valtteri.koivunen@hotmail.com>
2021-11-24 17:00:05 +0200

    viimeistely

--- dataset-collector.py
+++ dataset-collector.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import np
+import pyautogui
 
 username = input("Enter ip-camera username: ")
 password = input("Enter ip-camera password: ")
@@ -11,7 +12,8 @@ alakansioninkramentti = 1
 haluttukuvamaara = input("Syötä tallennettavien kuvien lukumäärä: ")
 kuvienkokonaismaara = 0
 alakansioidenkokonaismaara = 0
-haluttuvarikanava = input("Värikanava vaihtoehdot: 1 = normaali, 2 = harmaa, 3 = punainen, 4 = vihreä, 5 = sininen, valitse värikanava: ")
+haluttuvarikanava = input(
+    "Värikanava vaihtoehdot: 1 = normaali, 2 = harmaa, 3 = punainen, 4 = vihreä, 5 = sininen, valitse värikanava: ")
 
 # pääkansion luominen
 if not os.path.exists(paakansio):
@@ -52,6 +54,20 @@ print("Alakansiot sisältävät: " + (str(kuvienkokonaismaara)) + " kuvaa")
 # luodaan while loop, joka jatkuu ikuisesti, eli kamera yhteys pysyy auki
 while True:
     ret, frame = cap.read()
+    # ratkaistaan visuaalisen syötteen tulolähteen leveys ja korkeus
+    kuvanleveys = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    kuvankorkeus = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    # ratkaistaa ruudun leveys, korkeus
+    ruudunleveys = pyautogui.size().width
+    ruudunkorkeus = pyautogui.size().height
+    resoluutio = str(kuvanleveys) + "x" + str(kuvankorkeus)
+    fontti = cv2.FONT_HERSHEY_SIMPLEX
+    kuvalaskuri = str(kuvainkramentti - kuvienkokonaismaara) + "/" + str(haluttukuvamaara)
+    cv2.putText(frame, kuvalaskuri, (10, int(kuvankorkeus) - 30), fontti, 0.5, (255, 255, 255), 1, cv2.LINE_4)
+    cv2.putText(frame, resoluutio, (10, int(kuvankorkeus) - 10), fontti, 0.5, (255, 255, 255), 1, cv2.LINE_4)
+    cv2.imshow("kameranäkymä", frame)
+    cv2.moveWindow("kameranäkymä", int(ruudunleveys / 2 - kuvanleveys / 2), int(ruudunkorkeus / 2 - kuvankorkeus / 2))
+
     mitoitus = cv2.resize(frame, (1024, 1024))
     harmaa = cv2.cvtColor(mitoitus, cv2.COLOR_BGR2GRAY)
     nollamatriisi = np.zeros(mitoitus.shape[:2], dtype="uint8")
@@ -60,7 +76,7 @@ while True:
     vihrea = cv2.merge([nollamatriisi, vihrea, nollamatriisi])
     sininen = cv2.merge([sininen, nollamatriisi, nollamatriisi])
     # nimetään kamera ikkuna, määritellään visuaalisen datan tulolähde
-    cv2.imshow("kameranäkymä", frame)
+
     if os.path.exists(paakansio + "/" + alakansio + (str(alakansioninkramentti))):
         kuvanimi = paakansio + "/" + alakansio + (str(alakansioninkramentti)) + "/%d.jpg" % kuvainkramentti
     else:
