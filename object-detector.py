72b616aeb7bfd0cdd1d84f28049206f38af5a6b5
valtteri <valtteri.koivunen@hotmail.com>
2021-11-29 20:08:42 +0200

    rajauslaatikon muokkaus

--- object-detector.py
+++ object-detector.py
@@ -15,6 +15,9 @@ else:
 
 # alkeellinen tunnistin, ohjelman käynnistämistä, kokeilua varten
 tunnistin = cv2.CascadeClassifier('custom-cascades\cascadetest.xml')
+# selvitetään ikkunan leveys sekä korkeus, perusteella määritellään loppupiste koordinaatit
+leveys = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+korkeus = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 while True:
     ret, frame = cap.read()
     #tunnistimen hienosäätö
@@ -22,7 +25,14 @@ while True:
     #laatikon piirtäminen objektin ympärille
     for (x, y, w, h) in objekti:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 1)
-
+        # viiva ylhäällä, lopetuspisteeksi voidaan määritellä ikuinen -y koordinaatti
+        cv2.line(frame, (int(x) + int(w/2), int(y)), (int(x) + int(w/2), int(-korkeus)), (255, 255, 255), 1)
+        # viiva vasemmalla, lopetuspisteeksi voidaan vääritellä ikuinen -x koordinaatti
+        cv2.line(frame, (int(x), int(y) + int(h/2)), (int(-leveys), int(y) + int(h/2)), (255, 255, 255), 1)
+        # viiva alhaalla, lopetuspisteeksi ei voida määritellä ikuista y koordinaattia
+        cv2.line(frame, (int(x) + int(w/2), int(y) + int(h)), (int(x) + int(w/2), int(korkeus)), (255, 255, 255), 1)
+        # viiva oikealla, lopetuspisteeksi ei voida määritellä ikuista x koordinaattia
+        cv2.line(frame, (int(x) + int(w), int(y) + int(h/2)), (int(leveys), int(y) + int(h/2)), (255, 255, 255), 1)
     cv2.imshow("objectdetection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
